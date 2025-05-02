3f5a9141ebf9f144df522247a19fd0635efe5dac
denisdiaconu <denisdiaconu117@yahoo.com>
2025-05-02 11:20:46 +0200

    Add redirect to sign in

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,20 @@
 import Header from '@/components/layout/header';
+import { auth } from '@clerk/nextjs/server';
 import React from 'react';
 
-export default function DashboardLayout({children}: { children: React.ReactNode}) {
-  return <div className='min-w-screen flex flex-col'>
-    <Header />
-    <main className='flex-1 container mx-auto p-4'>
-        {children}
-    </main>
-  </div>;
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { userId, redirectToSignIn } = await auth();
+
+  if (!userId) return redirectToSignIn();
+
+  return (
+    <div className="min-w-screen flex flex-col">
+      <Header />
+      <main className="flex-1 container mx-auto p-4">{children}</main>
+    </div>
+  );
 }
