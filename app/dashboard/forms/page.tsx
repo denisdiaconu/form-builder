2fc2d38141876290cc0d8384c6991cb3c444e629
denisdiaconu <denisdiaconu117@yahoo.com>
2025-05-05 11:38:42 +0200

    Add forms page

---
+++ app/dashboard/forms/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function FormPage() {
+  return (
+    <div className="space-y-6">
+      <div>
+        <h1 className='text-3xl font-bold'>My Forms</h1>
+        <p className='text-gray-500 mt-1'>Create and manage your forms</p>
+      </div>
+    </div>
+  );
+}
