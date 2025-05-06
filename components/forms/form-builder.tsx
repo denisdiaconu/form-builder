edf6d85123a638a03710f617ac0211b35a98ee80
denisdiaconu <denisdiaconu117@yahoo.com>
2025-05-06 11:05:05 +0200

    Add state to form builder

--- components/forms/form-builder.tsx
+++ components/forms/form-builder.tsx
@@ -1,3 +1,15 @@
+import { useState } from 'react';
+
 export default function FormBuilder() {
+  const [form, setForm] = useState({
+    title: '',
+    description: '',
+    questions: [
+      {
+        id: '1',
+        text: '',
+      },
+    ],
+  });
   return <div>FormBuilder</div>;
 }
