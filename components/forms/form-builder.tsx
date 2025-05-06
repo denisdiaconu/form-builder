5ac86bc9eba3117bd8b1273dfb3baeb9e4f2b773
denisdiaconu <denisdiaconu117@yahoo.com>
2025-05-06 11:19:41 +0200

    Add label, input and textarea to form builder

--- components/forms/form-builder.tsx
+++ components/forms/form-builder.tsx
@@ -1,6 +1,8 @@
+'use client';
 import { useState } from 'react';
 import { Label } from '../ui/label';
 import { Input } from '../ui/input';
+import { Textarea } from '../ui/textarea';
 
 export default function FormBuilder() {
   const [form, setForm] = useState({
@@ -13,12 +15,30 @@ export default function FormBuilder() {
       },
     ],
   });
-  return <form className='space-y-8'>
-    <div>
-      <div>
-        <Label>Title</Label>
-        <Input />
+  return (
+    <form className="space-y-8">
+      <div className="space-y-4">
+        <div>
+          <Label htmlFor="title">Title</Label>
+          <Input
+            className="mt-1"
+            id="title"
+            value={form.title}
+            onChange={(e) => setForm({ ...form, title: e.target.value })}
+            placeholder="Enter form title"
+          />
+        </div>
+        <div>
+          <Label htmlFor="description">Description (Optional)</Label>
+          <Textarea
+            className="mt-1"
+            id="description"
+            value={form.description}
+            onChange={(e) => setForm({ ...form, description: e.target.value })}
+            placeholder="Enter form description"
+          />
+        </div>
       </div>
-    </div>
-  </form>;
+    </form>
+  );
 }
