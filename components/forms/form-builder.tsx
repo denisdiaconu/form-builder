d844ff1d95c695202736e532e6d687ef0bbd48ec
denisdiaconu <denisdiaconu117@yahoo.com>
2025-05-07 12:20:25 +0200

    Add buttons to form

--- components/forms/form-builder.tsx
+++ components/forms/form-builder.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { Label } from '../ui/label';
 import { Input } from '../ui/input';
 import { Textarea } from '../ui/textarea';
@@ -16,8 +16,12 @@ export default function FormBuilder() {
       },
     ],
   });
+  
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+  }
   return (
-    <form className="space-y-8">
+    <form onSubmit={handleSubmit} className="space-y-8">
       <div className="space-y-4">
         <div>
           <Label htmlFor="title">Title</Label>
@@ -39,38 +43,40 @@ export default function FormBuilder() {
             placeholder="Enter form description"
           />
         </div>
-        <div className="space-y-6">
-          <div className="flex items-center justify-between">
-            <h3 className="text-lg font-medium">Questions</h3>
-            <Button variant="outline" type="button">
-              Add Question
-            </Button>
-          </div>
-          {form.questions.map((question, index) => (
-            <div key={question.id} className="space-y-2 p-4 border rounded-md">
-              <div className="flex items-center justify-between">
-                <Label htmlFor={`Question-${index}`}>
-                  Question {index + 1}
-                </Label>
-                <Button
-                  className="text-red-500 hover:text-red-700"
-                  type="button"
-                  size="sm"
-                  variant="ghost"
-                >
-                  remove
-                </Button>
-              </div>
-              <Textarea
-                className="mt-1"
-                id={`Question-${index}`}
-                value={question.text}
-                onChange={(e) => {}}
-                placeholder="Enter your question"
-              />
-            </div>
-          ))}
+      </div>
+      <div className="space-y-6">
+        <div className="flex items-center justify-between">
+          <h3 className="text-lg font-medium">Questions</h3>
+          <Button variant="outline" type="button">
+            Add Question
+          </Button>
         </div>
+        {form.questions.map((question, index) => (
+          <div key={question.id} className="space-y-2 p-4 border rounded-md">
+            <div className="flex items-center justify-between">
+              <Label htmlFor={`Question-${index}`}>Question {index + 1}</Label>
+              <Button
+                className="text-red-500 hover:text-red-700"
+                type="button"
+                size="sm"
+                variant="ghost"
+              >
+                remove
+              </Button>
+            </div>
+            <Textarea
+              className="mt-1"
+              id={`Question-${index}`}
+              value={question.text}
+              onChange={(e) => {}}
+              placeholder="Enter your question"
+            />
+          </div>
+        ))}
+      </div>
+      <div className='flex justify-end gap-2'>
+        <Button type='button' variant='outline' onClick={() => {}}>Cancel</Button>
+        <Button type='submit'>Save</Button>
       </div>
     </form>
   );
