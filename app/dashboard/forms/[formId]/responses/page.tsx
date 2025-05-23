1036aed11c2a7412733f54fb8d6c75f0dbc48963
denisdiaconu <denisdiaconu117@yahoo.com>
2025-05-23 11:37:38 +0200

    Add params to responses page

--- app/dashboard/forms/[formId]/responses/page.tsx
+++ app/dashboard/forms/[formId]/responses/page.tsx
@@ -1,5 +1,25 @@
-export default function FormResponsesPage() {
+import { auth } from "@clerk/nextjs/server";
+
+export default async function FormResponsesPage({
+  params,
+}: {
+  params: Promise<{ formId: string }>;
+}) {
+    const { userId, redirectToSignIn } = await auth();
+      const { formId } = await params;
+    
+      if (!userId) return redirectToSignIn();
   return (
-    <div>page</div>
+    <div className="max-w-3xl mx-auto space-y-6">
+        <div className="flex items-center justify-between">
+            <div>
+                <h1 className="text-3xl font-bold">Form Responses</h1>
+                <p className="text-gray-500"></p>
+            </div>
+            <div>
+                button
+            </div>
+        </div>
+    </div>
   )
 }
