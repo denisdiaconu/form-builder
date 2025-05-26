7ab456db42365aed5da42ac5427396945a557902
denisdiaconu <denisdiaconu117@yahoo.com>
2025-05-26 12:06:55 +0200

    Add form preview component

--- app/forms/[formId]/page.tsx
+++ app/forms/[formId]/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import FormPreview from "@/components/forms/form-preview";
 
-export default function page() {
+export default function PublicFormPage() {
   return (
-    <div>page</div>
+    <div className="bg-gray-50 min-h-screen py-12">
+        <div className="bg-white container max-w-3xl mx-auto p-6 rounded-lg shadow">
+            <FormPreview />
+        </div>
+    </div>
   )
 }
