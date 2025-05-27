8e53d2fb24c1b5bc7ec9139e1d07fb8c2dfe9ba4
denisdiaconu <denisdiaconu117@yahoo.com>
2025-05-27 10:55:47 +0200

    Add name field to preview

--- components/forms/form-preview.tsx
+++ components/forms/form-preview.tsx
@@ -1,4 +1,8 @@
+'use client'
 import { Question } from '@/lib/generated/prisma';
+import { Label } from '../ui/label';
+import { Input } from '../ui/input';
+import { useState } from 'react';
 
 type FormPreviewProps = {
   form: {
@@ -9,6 +13,8 @@ type FormPreviewProps = {
   };
 };
 export default function FormPreview({ form }: FormPreviewProps) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   return (
     <div className="max-w-xl mx-auto">
       <div className="mb-8">
@@ -17,6 +23,18 @@ export default function FormPreview({ form }: FormPreviewProps) {
           <p className="mt-2 text-gray-600">{form.description}</p>
         )}
       </div>
+      <form>
+        <div className="space-y-4">
+          <Label>Your Name (Optional)</Label>
+          <Input
+            placeholder="Enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="mt-1"
+          />
+        </div>
+        
+      </form>
     </div>
   );
 }
